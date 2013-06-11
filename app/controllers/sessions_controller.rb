a9e146e9d0d0b7aebaa4ab0acfed794014d5f140
Anil de Mello <anil.demello@gmail.com>
2013-06-11 14:05:41 +0200

    End of athentication & sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
+if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
@@ -15,5 +16,8 @@ class SessionsController < ApplicationController
 
 
   def destroy
+    sign_out
+    redirect_to root_url
   end
+
 end
